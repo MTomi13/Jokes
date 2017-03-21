e488b93c7eceb09c889245d23feb1af98b9f9335
Tamas Marton <tamas.marton@hellobrandwidth.com>
2017-03-21 15:18:45 +0000

    create base of HomeActivity

--- MyApplication/app/src/main/java/com/marton/tamas/funnychuck/home/HomeActivity.java
+++ MyApplication/app/src/main/java/com/marton/tamas/funnychuck/home/HomeActivity.java
@@ -1,13 +1,15 @@
 package com.marton.tamas.funnychuck.home;
 
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 
 import com.marton.tamas.funnychuck.JokeApplication;
+import com.marton.tamas.funnychuck.R;
 import com.marton.tamas.funnychuck.dependencies.ActivityModule;
 
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 import dagger.ObjectGraph;
 
 /**
@@ -22,19 +24,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        setContentView(R.layout.home_activity);
+        ButterKnife.bind(this);
 
         activityGraph = ((JokeApplication) getApplicationContext()).getApplicationGraph().plus(new ActivityModule(this));
         inject(this);
     }
 
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        super.setContentView(layoutResID);
-        ButterKnife.bind(this);
-    }
-
     private void inject(Object object) {
         activityGraph.inject(object);
+    }
+
+    @OnClick(R.id.btn_random_joke)
+    private void onRandomJokeBtnClicked() {
+
+    }
+
+    @OnClick(R.id.btn_text_input)
+    private void onTextInputBtnClicked() {
+
+    }
+
+    @OnClick(R.id.btn_lazy_list)
+    private void onEndlessListBtnClicked() {
+
+    }
+
+    @OnCheckedChanged(R.id.deny_explicit)
+    private void onFilterChecked() {
+
     }
 
     @Override
